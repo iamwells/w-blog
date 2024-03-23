2e59964b312e627a5a375567900420ad93cebb31
iamwells <iamwells@163.com>
2024-03-23 17:58:29 +0800

    Initial commit

--- w-blog-admin/w-blog-admin-back/src/test/java/io/github/iamwells/admin/DatasourceTest.java
+++ w-blog-admin/w-blog-admin-back/src/test/java/io/github/iamwells/admin/DatasourceTest.java
@@ -38,38 +38,5 @@ public class DatasourceTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Test
-    public void testSelectUser() throws JsonProcessingException {
-        System.out.println();
-        UserWithRolesAndPermissions admin = userService.getUserWithRolesByUsername("admin");
-        System.out.println(admin);
-    }
 
-    @Test
-    public void testInsertUser() {
-        User user = new User();
-        user.setUsername("test4");
-        user.setPassword("123456");
-        user.setDeleted(0);
-        System.out.println(OffsetDateTime.now());
-        boolean save = userService.save(user);
-        System.out.println(user);
-    }
-
-    @Test
-    public void testJwt() {
-        JwtHeader jwtHeader1 = jwtHeader;
-        System.out.println();
-        User user = new User();
-        user.setUsername("admin");
-        user.setId(1L);
-    }
-
-    @Test
-    public void testPasswordEncoder() {
-        String encode = passwordEncoder.encode("123456");
-        UpdateWrapper<User> updateWrapper = new UpdateWrapper<User>()
-                .eq("user_username", "admin").set("user_password", encode);
-        userMapper.update(updateWrapper);
-    }
 }
