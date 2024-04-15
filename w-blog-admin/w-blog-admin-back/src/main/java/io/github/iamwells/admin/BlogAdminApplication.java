0601e1f06a3681e8f42fbc70cdfe3601805f7025
iamwells <iamwells@163.com>
2024-04-15 12:23:35 +0800

    add HanLP data

--- w-blog-admin/w-blog-admin-back/src/main/java/io/github/iamwells/admin/BlogAdminApplication.java
+++ w-blog-admin/w-blog-admin-back/src/main/java/io/github/iamwells/admin/BlogAdminApplication.java
@@ -1,12 +1,21 @@
 package io.github.iamwells.admin;
 
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
+@Slf4j
 public class BlogAdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BlogAdminApplication.class, args);
+        try {
+            SpringApplication.run(BlogAdminApplication.class, args);
+        } catch (Exception e) {
+
+        }
     }
 }
